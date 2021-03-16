b916e19c3575d56c7ea30938808238f1881e8825
leo.lu <leo.lu@blackshark.com>
2021-03-16 16:53:15 +0800

    Initial commit

---
+++ android/src/main/kotlin/tech/gentleflow/install_plugin/FileProvider.java
@@ -0,0 +1,4 @@
+package tech.gentleflow.install_plugin;
+
+public class FileProvider extends androidx.core.content.FileProvider {
+}
---
+++ example/android/app/src/main/kotlin/tech/gentleflow/install_plugin_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package tech.gentleflow.install_plugin_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
